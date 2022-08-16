95c0d51d5dcada9806face26fe1e76192a59a716
Jacopo Santarelli <jacoposantarelli@MBP-di-Jacopo.station>
2022-08-16 14:49:23 +0200

    Axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
-import logo from './logo.svg';
-import React, { Component }  from 'react';
+import React, {useEffect,useState }  from 'react';
 import './App.css';
 import Sidebar from './Sidebar/Sidebar';
 import Chat from './Chat/Chat';
+import Pusher from 'pusher-js';
+import axios from "./axios";
+
 
 function App() {
+
+  const [messages,setMessages]=useState([]);
+
+   //prende tutti i dati e le stampa tutte
+  useEffect(()=>{
+    axios.get("/api/v1/messages/synch").then((response)=>{
+      console.log(response.data);
+      setMessages(response.data);
+    }).catch();
+  },[])
+
+
+  useEffect(()=>{
+    console.log("Effect APP");
+
+    var pusher = new Pusher('6d3739549315b9cae674', {
+      cluster: 'eu'
+    });
+
+    var channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(newMessage) {
+      // alert(JSON.stringify(data));
+      //con ...messages andiamo a recuperare tutti i messaggi e far accodare quelli nuovi
+      // se omettiamo ... riceveremo un messaggio per volta
+      setMessages([...messages,newMessage]);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+
+    }
+
+  },[messages]);
+
   return (
     <div className="app">
       <div class="app__body">
       <Sidebar/>
-      <Chat/>
+      <Chat messages={messages}/>
       </div>
     </div>
   );
--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,14 +1,69 @@
-import React from 'react'
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, InsertEmoticon, MoreVert, SearchOutlined } from '@material-ui/icons';
+import axios from "../axios";
+import React, {useState} from 'react'
+import "./Chat.css";
 
-function Chat() {
-  return (
-    <div>
-        <h1>
-            Chat
-        </h1>
+function Chat({messages}) {
+
+  const [input,setInput] = useState("");
+
+  const sendMessage = async (e) =>{
+        e.preventDefault();
         
+        await axios.post("/api/v1/messages",{
+          message : input,
+          name : "Author Name",
+          timestamp : "now",
+          received : false
+        }).then();
+        
+        setInput("");
+  
+      };
+      
+  return (
+    <div className="chat">
+        <div className="chat__header">
+           <Avatar></Avatar>
+           <div className="chat__header_info">
+                <h3> Chat Name</h3>
+                <p>Visto l'ultima....</p>
+           </div>
+           <div className="chat__header_right">
+               <IconButton>
+                 <SearchOutlined></SearchOutlined>
+               </IconButton>
+               <IconButton>
+                 <AttachFile></AttachFile>
+               </IconButton>
+               <IconButton>
+                 <MoreVert></MoreVert>
+               </IconButton>
+           </div>
+        </div>
+        <div className="chat__body">
+          {messages.map((message) => (
+               <p key={message._id} class={`chat__message ${message.received && "chat__receiver"}`}>
+               <span className="chat__name">{message.name}</span>
+                     {message.message}
+               <span className="chat__timestamp">{message.timestamp}</span>
+               </p>
+          ))}
+        </div>
+        <div className="chat__footer">
+           <IconButton> 
+              <InsertEmoticon></InsertEmoticon>
+           </IconButton>
+           <form>
+              <input value={input}
+                onChange={(e)=> setInput(e.target.value)}
+              placeholder='Scrivi un messaggio...' type='text'></input>
+              <button onClick={sendMessage} type="submit">Invia</button>
+           </form>
+        </div>
     </div>
   )
 }
 
-export default Chat
+export default Chat;
